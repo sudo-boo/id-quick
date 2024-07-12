5c579f945c0bdd2e81bde06dd8cd26731b568e79
sudo-boo <22b0910@iitb.ac.in>
2024-07-12 22:45:01 +0530

    ADD: Only Portrait support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 // main.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:id_quick/pages/display_id_card_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
